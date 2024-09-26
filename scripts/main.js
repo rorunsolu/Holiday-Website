e6ae94a8b898b1b4b951de2e39939e3a095c30d9
Ray Orunsolu <rorunsolu@gmail.com>
2024-09-26 19:37:11 +0100

    overhaul of theme + favicon added

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+import '/styles/style.scss';
+import '/styles/typography.scss';
+import '/styles/utilities.scss';
+import '/styles/navbar.scss';
+import '/styles/header.scss';
+import '/styles/locations.scss';
+import '/styles/about.scss';
+import '/styles/activities.scss';
+import '/styles/packages.scss';
+import '/styles/footer.scss';
