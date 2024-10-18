1c644d584813ab68de91a1e6d81a5cb2a8b937ab
Ray Orunsolu <rorunsolu@gmail.com>
2024-10-18 21:38:41 +0100

    added icons to book now buttons

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,7 @@ import '/styles/style.scss';
 import '/styles/typography.scss';
 import '/styles/utilities.scss';
 import '/styles/navbar.scss';
+import '/styles/menu.scss';
 import '/styles/header.scss';
 import '/styles/locations.scss';
 import '/styles/about.scss';
